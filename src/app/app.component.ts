ea9b011a67e5491267ee80f557bb2da00e0e834e
Jeff Cadieux <jeffcadieux@hotmail.com>
2022-07-31 08:55:10 +0900

    Initial working demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'checkbox-test';
+  public isChecked: boolean = false;
+
+  public async onInputChange(event: any): Promise<void> {
+    if (!event.target.checked && this.shouldShowConfirmationDialog()) {
+      const dialogResult = await this.showConfirmationDialog();
+      if (dialogResult) {
+        setTimeout(() => { this.isChecked = true; }, 0);
+      }
+    }
+  }
+
+  private shouldShowConfirmationDialog(): boolean {
+    return true;
+  }
+
+  private showConfirmationDialog(): Promise<boolean> {
+    return Promise.resolve(true);
+  }
 }
